0ec017d175d2ddd13ec91f444a1920db3b9c88ab
ayoub adil <55322505+ayoubad12@users.noreply.github.com>
2024-02-26 00:13:44 +0100

    product description

---
+++ src/app/layout/item/item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './item.component.html',
+  styleUrl: './item.component.scss'
+})
+export class ItemComponent {
+
+}
