38c72a17d2d5ad1a2aa7bcdadf6fa0ab6d8ea99c
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-02-26 16:51:29 +0100

    add not found ui

--- src/app/shared/not-found/not-found.component.ts
+++ src/app/shared/not-found/not-found.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,ButtonComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
 export class NotFoundComponent {
-
+  goBack() {
+    window.history.back();
+  }
 }
