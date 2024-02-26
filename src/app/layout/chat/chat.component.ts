0085217ff9bb33f3a3c2d569bd0b31dd73eb1cd0
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-02-26 15:18:57 +0100

    add chat drawer

--- src/app/layout/chat/chat.component.ts
+++ src/app/layout/chat/chat.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { InputComponent } from '../../components/input/input.component';
+import { ButtonComponent } from '../../components/button/button.component';
+import { FormsModule } from '@angular/forms';
+import { Timestamp } from 'firebase/firestore';
+import { StorageService } from '../../core/data/storage.service';
+import { Message } from '../../core/models/message';
+import { ChatService } from '../../core/services/chat.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [InputComponent, ButtonComponent, FormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  messageChat = '';
+  private chatService: ChatService = inject(ChatService);
+  private storage: StorageService = inject(StorageService);
+  messages: any[] = [];
+  text = '';
+  message: Message = {
+    content: this.text,
+    sender: 'this.storage.user.lastName',
+    timestamp: Timestamp.now()
+  };
+  subscription: Subscription | undefined;
+  send() {
+    this.chatService.createMessage(this.message);
+    console.log("added")
+  }
+  async ngOnInit() {
+    await this.chatService.getMessages().then((items) => {
+      this.messages = items;
+    });
+  }
+
+
+
+
+
 }
--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChatComponent } from '../chat/chat.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ChatComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
