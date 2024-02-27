39b6b09d2e33b33bf7f91ce0db24adefad49b326
EL MOUADDIBE <elmouaddibezaid@gmail.com>
2024-02-28 00:21:41 +0100

    fix cards - explore page

--- src/app/layout/explore/explore.component.ts
+++ src/app/layout/explore/explore.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardEventComponent } from '../../components/card-event/card-event.component';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-explore',
   standalone: true,
-  imports: [CardEventComponent,RouterLink],
+  imports: [CardEventComponent,RouterLink,CommonModule],
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.scss'
 })
