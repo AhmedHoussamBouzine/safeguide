f285eeb1064ba9dd1b965d9e86875781f72fff03
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-02-27 14:39:54 +0100

    fix explore design

--- src/app/layout/explore/explore.component.ts
+++ src/app/layout/explore/explore.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CardEventComponent } from '../../components/card-event/card-event.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-explore',
   standalone: true,
-  imports: [CardEventComponent],
+  imports: [CardEventComponent,RouterLink],
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.scss'
 })
 export class ExploreComponent {
-  
+
 }
