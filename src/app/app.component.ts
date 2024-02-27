22308aa8fcc6b47cf69f041969af7e5675fd6504
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-02-27 11:24:53 +0100

    fix auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
+import { Component, OnDestroy, OnInit, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NavigationEnd, Router, RouterOutlet } from "@angular/router";
+import { Auth } from "@angular/fire/auth";
+import { StorageService } from "./core/data/storage.service";
+import { Subscription } from "rxjs";
+import { AuthentificationService } from "./core/services/authentification.service";
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,5 +17,27 @@ import { FooterComponent } from "./shared/footer/footer.component";
 })
 export class AppComponent {
   title = 'safeguide';
+  auth$!: Subscription;
+  constructor() {}
+
+  private authentication = inject(AuthentificationService);
+  private auth = inject(Auth);
+  private storage = inject(StorageService);
+  private router = inject(Router);
+
+  ngOnInit() {
+    // Load user data
+    this.auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.auth$ = this.authentication
+          .getUser(user.uid)
+          .subscribe((userData) => {
+            this.storage.user = userData;
+          });
+        return;
+      }
+      this.storage.user = undefined;
+    });
+  }
 
 }
