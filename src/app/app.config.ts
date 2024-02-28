e3b5d048c1bb5aa79062fbb23e6f11f46ff4e8e3
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-02-28 19:46:15 +0100

    add pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -11,14 +11,21 @@ import { environment } from "../environments/environment.development";
 import { provideToastr } from "ngx-toastr";
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-  provideClientHydration(),
-  importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebaseConfig))),
-  importProvidersFrom(provideAuth(() => getAuth())),
-  importProvidersFrom(provideFirestore(() => getFirestore())),
-  importProvidersFrom(provideDatabase(() => getDatabase())),
-  provideAnimations(),
-  provideToastr()]
+    provideClientHydration(),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebaseConfig))),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    provideAnimations(),
+    provideToastr(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
